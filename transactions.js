dfbadbad9b7ca3ee689631186d51d03d7843edef
Joel Swenddal <swenddaj@flip2.engr.oregonstate.edu>
2021-11-22 11:22:50 -0800

    Added code for DELETE transaction -- not in outline so commented out

--- transactions.js
+++ transactions.js
@@ -34,6 +34,7 @@ module.exports = function () {
     router.get('/', function (req, res) {
         let callbackCount = 0;
         let context = {};
+        context.jsscripts = ["deletetransaction.js"];
         let mysql = req.app.get('mysql');
         getTransactions(res, mysql, context, complete);
         getID(res, mysql, context, complete);
@@ -50,7 +51,7 @@ module.exports = function () {
         console.log(req.body)
 
         let mysql = req.app.get('mysql');
-        let sql = "INSERT INTO transactions (account_ID, date_time , amount, transaction_type) VALUES (? , ?, ?)";
+        let sql = "INSERT INTO transactions (account_ID, amount, transaction_type) VALUES (? , ?, ?)";
         let inserts = [req.body.accountID, req.body.amount, req.body.transactionType];
         sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
             if (error) {
@@ -62,6 +63,26 @@ module.exports = function () {
             }
         });
     });
+
+    /* For dev - Route to delete a transaction, simply returns a 202 upon success. Ajax will handle this. */
+    /*
+    router.delete('/:transaction_id', function (req, res) {
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM transactions WHERE transaction_ID = ?";
+        var inserts = [req.params.transaction_id];
+        sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+            if (error) {
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            } else {
+                res.status(202).end();
+            }
+        })
+    });
+    */
+
 
     return router;
 }();
